dcfbcb7c4036cb972880b44cc46709b05a7727de
granth2023 <grantharris38@gmail.com>
2024-01-29 13:33:44 -0500

    new

---
+++ intro.py
@@ -0,0 +1,18 @@
+print("hello world!")
+
+
+my_var = "variable"
+
+# no undefined
+print(my_var)
+
+my_var = 4
+print(my_var)
+
+MY_CONSTANT_VARIABLE = 10 
+MY_CONSTANT_VARIABLE = 9 
+
+#every piece of data in python is an object  
+#this is the same as javascript
+
+print(type(my_var))
